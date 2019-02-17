98a512f9c19a9d3cd3b463b86d16dd2448719a58
Stanislav <stasn@ua.fn>
2019-02-17 18:04:47 +0300

    first commit

---
+++ src/main/java/com/text/web/textWebAplication/TextWebAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.text.web.textWebAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextWebAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextWebAplicationApplication.class, args);
+	}
+
+}
